94af4195acab0ba5e44f80f0bcc811547f274595
이웅희 <eunvanz@gmail.com>
2017-12-25 20:05:03 +0900

    - v.0.3.15 - 진정한웅이 스테이지 추

--- src/routes/Adventure/components/AdventureView.js
+++ src/routes/Adventure/components/AdventureView.js
@@ -7,8 +7,6 @@ import ScrollArea from 'react-scrollbar'
 
 import { getMsg } from 'utils/commonUtil'
 
-import { colors } from 'constants/colors'
-
 import ContentContainer from 'components/ContentContainer'
 import UserInfo from '../../Battle/components/UserInfo'
 import MonCard from 'components/MonCard'
@@ -19,6 +17,7 @@ import User from 'models/user'
 
 import doctorOh from './assets/doctor_oh.png'
 import woongImg from '../../../components/TutorialModal/assets/KakaoTalk_2017-12-21-11-30-42_Photo_14.png'
+import realWoongImg from '../../../components/TutorialModal/assets/KakaoTalk_Photo_2017-12-19-01-31-13.png'
 
 class AdventureView extends React.Component {
   constructor (props) {
@@ -63,6 +62,8 @@ class AdventureView extends React.Component {
       return Object.assign({}, new User(), { nickname: getMsg(messages.adventure.trainerName[0], locale), profileImage: doctorOh })
     } else if (user.stage <= 80) {
       return Object.assign({}, new User(), { nickname: getMsg(messages.adventure.trainerName[1], locale), profileImage: woongImg, enabledHonors: [{ burf: [5, 5, 5, 5, 5, 5], name: '특급트레이너', type: 1, id: '-L-MK6QhWji8QtK0kQ8Y', reward: 50, condition: 100 }] })
+    } else if (user.stage <= 100) {
+      return Object.assign({}, new User(), { nickname: getMsg(messages.adventure.trainerName[2], locale), profileImage: realWoongImg, enabledHonors: [{ burf: [10, 10, 10, 10, 10, 10], name: '초급짐리더', type: 1 }, { burf: [0, 0, 0, 4, 0, 6], name: '바위애호가', type: 2 }] })
     }
   }
   _getTrainerImg (stage) {
